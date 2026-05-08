a31d6482264906a67db788f0088106a2615cc0b9
sarraissatou0905-ux <sarr.aissatou0905@gmail.com>
2026-05-08 14:05:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,60 @@
 
+import streamlit as st
+
+
+st.header("TECHNICIENNE SUPERIEURE EN AGRONOMIE ET AGROALIMENTAIRE ")
+st.write("_________________________________")
+st.subheader("A PROPOS DE MOI")
+with st.sidebar:
+     st.image("profil.jpeg",width=180)
+     st.write("MAIL=sarr.aissatou1@uam.edu.sn")
+     st.write("ADDRESS=GUEDIAWAYE,WAKHINANE NIMZATT")
+     st.write("_____________________________")
+     
+     st.markdown(""" Disponible pour un entretien, vous pouvez me joindre facilement par email""")
+     st.write("_______________________________")
+     st.header("FORMATION")
+     st.markdown("""
+      LICENCE  EN SCIENCE ET TECHNIQUE AGRICOLES ALIMENTAIRES ET NUTRITIONELLES
+
+      BACCALAURIAT SCIENTIFIQUE """)
+
+
+st.markdown("""Technicienne superieur en agronomie et en agroalimentaire passionnée par l'agroécologie.
+      je souhaite contribuer à des projets durables favorisant la biodiversité et la sécurité alimentaire.
+      Curieuse, motivée et rapide à apprendre, j'ai déjà participé à un stage d'un mois où j'ai acquis des bases pratiques en gestion des sols et collecte de données.
+       Mon objectif est de développer mes compétences au sein d'organisations engagées pour un avenir agricole résilient. """)
+     
+st.write("______________________________________________")
+
+st.header("EXPERIENCE PROFESSIONNELLE")
+st.subheader("Stagiaire les jardins de GOULOUMBOU,TAMBACOUNDA AOUT-SEPTEMBRE 2025") 
+st.markdown("""
+
+       •	Mise en œuvre de pratiques agronomique de base (rotation des cultures, compostage, gestion durable des sols).
+
+       •	Observation et suivi des cultures et des sols.
+
+       •	 """)
+       
+st.subheader("Activités de terrain  Club Environnement (Eco Human Club) ")
+st.markdown("""Organisation et participation à des activités de sensibilisation environnementale.
+
+Mise en place de parcelles de démonstration et entretien des champs.""") 
+st.write("___________________________________________________")
+st.header("COMPETENCES")
+st.markdown("""J'ai acquis des pratiques agronomiques de base telles que le compostage et la gestion durable des sols.
+
+Je sais installer des systèmes d'irrigation (goutte-à-goutte).
+
+Je maîtrise la préparation des planches de culture et l'organisation des parcelles.
+
+je maitrise les logiciels du pack office.
+
+CANVA(Conception de visuels, présentations et supports de communication.)
+
+streamlit(Développement d'interfaces interactives et visualisation de données.)
+
+QGIS(Analyse spatiale, création de cartes thématiques.)
+
+)""")
